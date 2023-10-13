adf611a89334d449a5bd53413da3ec878ff30e19
André Rodrigues <andre.rodrigues0018@gmail.com>
2023-10-13 16:26:51 -0300

    feat: send audio to openai using axios and multer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 const multer = require('multer');
+const FormData = require('form-data');
 const fs = require('fs');
 
 const app = express();
@@ -9,7 +11,16 @@ const port = 3000; // Change this to the desired port number
 app.use(express.json());
 
 // Set up a file upload middleware using multer
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    // Generate a new file name with the .mp3 extension
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.mp3');
+  },
+});
+const upload = multer({ storage: storage });
+module.exports = upload;
 
 // Define a route to handle file uploads
 app.post('/upload', upload.single('file'), async (req, res) => {
@@ -19,6 +30,8 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     if (!mp3File) {
       return res.status(400).json({ error: 'No file uploaded' });
     }
+		console.log(mp3File)
+		const transcription = await transcribeGenerate(mp3File)
 
     // Remove the temporary file after it's sent
     fs.unlinkSync(mp3File.path);
@@ -26,9 +39,34 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     res.json({ message: 'File uploaded and sent successfully', file: mp3File });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({ error: error });
   }
 });
+
+async function transcribeGenerate(file){
+	filePath = file.destination + file.filename;
+	const formData = new FormData();
+	const newfile = fs.createReadStream(filePath)
+	formData.append('file', newfile);
+	formData.append('model', 'whisper-1');
+	
+	try {
+		const retorno = await axios({
+			method: 'post',
+			url: 'https://api.openai.com/v1/audio/transcriptions',
+			headers: {
+				'Authorization': `Bearer ${process.env.OPENAI_KEY}`,
+				...formData.getHeaders(),
+			},
+			data: formData,
+		})
+
+		console.log(retorno.data.text)
+	} catch (error) {
+		throw new Error(error);
+	}
+
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
